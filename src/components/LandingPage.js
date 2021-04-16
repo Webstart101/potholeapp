d39593c3e4de062497bd144a39155a7f7c314544
AntonioWorkman <antonio@jillandee.com>
2021-04-16 15:59:43 -0400

    fix: remove generic logo

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,13 +8,6 @@ function LandingPage() {
       <section className="aw-header bg-white border-b border-gray-200 shadow-md">
         <header className="container mx-auto flex items-center justify-between flex-wrap px-2 py-4 lg:p-6 ">
           <div className="aw-header__logo flex items-center flex-shrink-0 text-purple-700 hover:text-purple-900 mx-2 lg:mr-6 order-1 transition">
-            <svg
-              className="fill-current h-8 w-8 mr-2"
-              viewBox="0 0 54 54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
             <span className="font-semibold text-lg tracking-tight">
               Road Side Reporting
             </span>
